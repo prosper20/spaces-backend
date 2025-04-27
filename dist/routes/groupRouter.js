d26cdceedbab21827cd4daf3ca5e78a9d0508987
prosper20 <bobsonconnect@gmail.com>
2025-04-27 16:05:54 +0100

    chore: task routes and controllers

--- dist/routes/groupRouter.js
+++ dist/routes/groupRouter.js
@@ -5,7 +5,7 @@ var groupsController_1 = require("../controllers/groupsController");
 var verifyJWT_1 = require("../middleware/verifyJWT");
 var groupRouter = (0, express_1.Router)();
 groupRouter.post("/groups", verifyJWT_1.verifyJWT, groupsController_1.createGroup);
-groupRouter.get("/groups", groupsController_1.getGroupsByUser);
+groupRouter.get("/groups", verifyJWT_1.verifyJWT, groupsController_1.getGroupsByUser);
 groupRouter.get("/groups/:groupId", groupsController_1.getGroupById);
 groupRouter.get("/groups/:groupId/dashboard", groupsController_1.getGroupDashboardData);
 exports.default = groupRouter;
