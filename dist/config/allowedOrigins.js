741128ce0dd2b0e2b90484b73ff9d704ec169bee
Bobson Prosper Paebi <bobsonconnect@gmail.com>
2025-04-30 04:07:48 +0100

    Delete dist directory

--- dist/config/allowedOrigins.js
+++
@@ -1,6 +0,0 @@
-// export const allowedOrigins = [
-//     "http://127.0.0.1:3000",
-//     "https://spaces-frontend-lovat.vercel.app",
-//     "http://localhost:3000",
-// ]
-//# sourceMappingURL=allowedOrigins.js.map
